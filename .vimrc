3899d5e1355d482da74b1c8f9444a9212439f098
Nelson Walker <walkernelsoncode@gmail.com>
2022-08-23 18:16:42 +1000

    initial commit

---
+++ .vimrc
@@ -0,0 +1,13 @@
+set number
+set relativenumber
+syntax on
+set autoindent
+
+set hlsearch
+
+nnoremap <silent> <C-l> :nohl<CR>
+
+set ignorecase
+
+set tabstop=4
+set shiftwidth=4
---
+++ setup.sh
@@ -0,0 +1,8 @@
+ln -s ~/.dotfiles/zsh/.zshrc .zshrc
+
+mv ~/.config/regolith2 ~/.dotfiles/regolith2
+
+ln -s ~/.dotfiles/regolith2 ~/.config/regolith2
+
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
