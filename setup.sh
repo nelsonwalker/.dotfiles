2154ac3fb0caca0e4d4b9a1a29c705d2a35f4ba6
nelson <walkernelsoncode@gmail.com>
2022-12-18 17:52:55 +1100

    improve setup and tweak i3 settings

--- setup.sh
+++ setup.sh
@@ -1,10 +1,29 @@
+# install:
+# - curl
+# - regolith
+# - spotify
+# - vim
+# - git
+# - zsh
+# - i3lock
+# - dconf editor (to swap esc & capslock)
+# - ohmyzsh
+# - powerlevel10k
+# - zsh-vi-mode
+# - setup media keys in keyboard shortcuts
+# - fzf
+
 ln -s ~/.dotfiles/zsh/.zshrc .zshrc
 
-mv ~/.config/regolith2 ~/.dotfiles/regolith2
-
-ln -s ~/.dotfiles/regolith2 ~/.config/regolith2
+ln -s ~/.dotfiles/regolith2 ~/.config/
 
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+
+sudo apt remove regolith-i3-session
+sudo apt remove regolith-i3-navigation
+sudo apt remove regolith-i3-workspace-config
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -136,3 +136,5 @@ zvm_after_init_commands+=(source_fzf)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
